b57e18add904926a7d5335b0913385c29e5a457a
jahidrayhan1848 <rayhan.jahid1848@gmail.com>
2024-02-19 08:37:55 +0600

    grand total section done

--- js/ticket.js
+++ js/ticket.js
@@ -15,7 +15,7 @@ for (const seat of seatsButton){
             // current ticket ///
             const currentTickets=document.getElementById('current-ticket');
             const currentTicket=parseInt(currentTickets.innerText)
-            console.log(currentTicket);
+           
              const ticket=currentTicket -1 ;
             //  console.log(ticket);
             currentTickets.innerText=ticket
@@ -32,20 +32,44 @@ for (const seat of seatsButton){
              <td>${ticketPrice}</td>
              `;
              tbody.appendChild(tr)
-              
-            
+
+            //  total price ///
+            const totalPriceText=document.getElementById('total-price');
+            const GrandTotal=document.getElementById('grand-total');
+            const price =parseInt(totalPriceText.innerText);
+            const totalPrice= price + ticketPrice;
+            totalPriceText.innerText=totalPrice
+            GrandTotal.innerText=totalPrice;   
          }
          else{
             alert('No Allow')
-         }
-      
-          
-
-      
-       
-
-
-        
+         }   
 
     })
 }
+
+
+    //  discount offer ////
+    document.getElementById('apply').addEventListener('click',function (){
+    const couponId=document.getElementById('coupon').value;
+    const grandTotalText=document.getElementById('grand-total');
+    const grandTotal=grandTotalText.innerText;
+    if(couponId==='NEW15'){
+        const discount= 15 * grandTotal / 100;
+        const totalDiscount= grandTotal -discount;
+      grandTotalText.innerText=totalDiscount;
+    //   discount amount show 
+    const discountAmountId=document.getElementById('discount-amount')
+    const discountAmount=parseInt(discountAmountId.innerText);
+    const totalAmount= discount + discountAmount;
+    discountAmountId.innerText= totalAmount
+    //    discount done ///
+    const discountApply=document.getElementById('discount-done');
+    discountApply.classList.add('hidden')
+
+
+    }
+
+
+
+    })
